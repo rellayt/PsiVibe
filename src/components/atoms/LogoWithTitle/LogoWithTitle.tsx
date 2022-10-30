aa736ed80719a4da4866b2075c5b554aeda46c1f
RLT <rellayt09s@gmail.com>
2022-10-30 14:35:34 +0100

    first commit

---
+++ src/components/atoms/LogoWithTitle/LogoWithTitle.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+import { Wrapper } from './LogoWithTitle.styles';
+import {LogoProps} from "../Logo/Logo";
+
+const LogoWithTitle = ({}: LogoProps): ReactElement => {
+
+    return (
+        <Wrapper></Wrapper>
+    )};
+
+export default LogoWithTitle
